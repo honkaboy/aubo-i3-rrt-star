1892f08efb6e31c8bed67aa2b574541cc7a9bd9a
Nathaniel Honka <nhonka@apple.com>
2020-04-05 10:10:10 -0700

    cleanup

--- source/inc/tree.h
+++ source/inc/tree.h
@@ -14,21 +14,48 @@ class Tree {
 
   static constexpr NodeID kNone = -1;
 
+  // Add a node to the tree.
   NodeID Add(const Node& new_node, bool is_goal = false);
+
+  // Returns true if the tree is its predefined capacity.
   bool IsFull() const;
+
+  // Find nodes in the tree at most \p radius from \position according to
+  // distance_metric_.
   std::vector<NodeID> near_idxs(const Joint& position, double radius) const;
+
+  // Find the nearest node to \position according to distance_metric_.
   NodeID nearest(const Joint& position) const;
-  Node GetNode(const NodeID node_id) const;
+
+  // Get the location of the current "best" node (according to distance_to_goal).
   Joint GetBestNodePosition() const;
-  std::vector<NodeID> Solution() const;
+
+  // Node accessor.
+  Node GetNode(const NodeID node_id) const;
+
+  // Node setter.
   void SetNode(const NodeID node_id, const Node& node);
+
+  // Return the solution, if any exists, with the lowest cost.
+  std::vector<NodeID> Solution() const;
+
+  // Print a debug report about the tree.
   void Report() const;
 
  private:
+  // A metric to measure distances between nodes.
   const std::function<double(const Joint&, const Joint&)> distance_metric_;
+
+  // The maximum node storage allowed for the tree.
   const size_t kMaxNodes;
+
+  // Stores tree node data.
   std::vector<Node> nodes_;
+
+  // The IDs of nodes that have reached the goal.
   std::vector<NodeID> goal_node_idxs_;
+
+  // The best node found so far and a distance to goal (agnostic to distance metric).
   std::pair<NodeID, double> best_node_and_distance_to_goal_;
 };
 
